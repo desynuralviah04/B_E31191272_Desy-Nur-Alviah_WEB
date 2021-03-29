37a96667d3a4c629590ad189466d8f57fee9ef28
desynuralviah04 <desyalviah04@gmail.com>
2021-03-29 21:37:23 +0700

    tugas

---
+++ minggu8/ci/application/views/profil.php
@@ -0,0 +1,12 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>Profil</title>
+</head>
+<body>
+	<h2>Halaman Profil</h2>
+	<p> Selamat datang ini adalah halaman profil </p>
+</body>
+</html>
