7d01cf355417eb43100205f3cf64871db73cc797
desynuralviah04 <desyalviah04@gmail.com>
2021-04-16 15:16:29 +0700

    Library

---
+++ minggu10/application/views/contact.php
@@ -0,0 +1,12 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>Contact</title>
+</head>
+<body>
+	<h2>Halaman Contact</h2>
+	<p> Selamat datang ini adalah halaman contact </p>
+</body>
+</html>
---
+++ minggu10/application/views/latihan3view.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>Desy Nur Alvia</title>
+</head>
+<body>
+	<h2>Hello World dari CI Model</h2>
+	<h3> Menggunakan Controllers,model dan View</h3>
+</body>
+</html>
